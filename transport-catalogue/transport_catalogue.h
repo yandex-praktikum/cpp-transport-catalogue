228084c6f7a1180f4faab997cd80eb76d0ec9278
Евгений <79659107+echo9et@users.noreply.github.com>
2022-12-04 08:30:48 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -1,1 +1,72 @@
-// место для вашего кода
+#pragma once
+
+#include <deque>
+#include <string>
+#include <string_view>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+
+#include "geo.h"
+
+namespace TransportCatalogue {
+
+    struct Stop{
+        std::string name;
+        double width;
+        double longitude;
+    };
+
+    struct Router{
+        std::string name;
+        std::vector<Stop*> stops;
+    };
+
+    struct Bus{
+        std::string name;
+        std::vector<std::string> stops;
+    };
+
+    struct RouterInfo{
+        RouterInfo(std::string name){
+            this->name = name;
+        }
+        std::string name;
+        size_t counterStops = 0;
+        size_t counterUniqueStops = 0;
+        int lenghtRouter = 0;
+        double curvature = .0;
+    };
+
+    struct StopInfo{
+        StopInfo(std::string name){
+            this->name = name;
+        }
+        std::string name;
+        std::set<std::string> buses;
+        bool isFound = false;
+    };
+
+    class transport_catalogue
+    {
+    public:
+        transport_catalogue();
+        void load_stop(const Stop&);
+        void load_router(const Bus&);
+        RouterInfo getRouteInfo(const std::string& router) const;
+        StopInfo getStopInfo(const std::string& stop);
+        bool addDistance(std::string_view out,std::string_view to,const size_t distance);
+
+    private:
+        std::deque<Stop> _stops;
+        std::unordered_map<std::string_view, Stop*> _stopname_to_stop;
+        std::deque<Router> _routers;
+        std::unordered_map<std::string_view, Router*> _busname_to_router;
+        std::unordered_map<Stop*,std::unordered_map<Stop*, size_t>> _distance;
+
+        size_t getDistance(Stop* from, Stop* to) const;
+
+    };
+
+}
