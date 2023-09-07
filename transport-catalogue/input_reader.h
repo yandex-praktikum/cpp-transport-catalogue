da36dd1f79723c1a3e2a7937df1d07ba4c4209d6
PavelMalov <122271765+PavelMalov@users.noreply.github.com>
2023-09-07 12:16:25 +0300

    Update input_reader.h

--- transport-catalogue/input_reader.h
+++ transport-catalogue/input_reader.h
@@ -1,1 +1,29 @@
-// место для вашего кода
+#pragma once
+
+#include <deque>
+#include <string>
+#include <string_view>
+#include <sstream>
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+
+#include "transport_catalogue.h"
+
+namespace input {
+
+class Parser {
+ public:
+  void ParseQuery(std::string_view query);
+  void ParseStopQuery(std::string_view query, TransportCatalogue& catalogue);
+  void ParseBusQuery(std::string_view query, TransportCatalogue& catalogue);
+  void ParseAllQueries(TransportCatalogue& catalogue);
+  void ParseCicleBusQuery(std::string_view query, TransportCatalogue& catalogue);
+
+ private:
+  std::deque<std::string> deque_queries;
+};
+
+}
+
