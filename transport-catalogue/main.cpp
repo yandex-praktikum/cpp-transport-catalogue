57e806c90b0553b5a9d65a66a1ef3f734b1a9b15
PavelMalov <122271765+PavelMalov@users.noreply.github.com>
2023-09-07 12:16:48 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,1 +1,32 @@
-// место для вашего кода
+#include <iostream>
+#include <string>
+
+#include "input_reader.h"
+#include "stat_reader.h"
+
+int main() {
+  //std::ifstream inputFile("input.txt");
+  int N;
+  std::cin >> N;
+  //inputFile >> N;
+  //std::ofstream file("output.txt");
+  //inputFile.ignore();
+  std::cin.ignore();
+  std::string query;
+  TransportCatalogue catalogue;
+  input::Parser parser;
+  for (int i = 0; i < N; ++i) {
+    std::getline(std::cin, query);
+    parser.ParseQuery(query);
+  }
+  parser.ParseAllQueries(catalogue);
+  int K;
+  std::cin >> K;
+  std::cin.ignore();
+  for (int i = 0; i < K; ++i) {
+    std::getline(std::cin, query);
+    output::ParseQuery(query, catalogue);
+  }
+  //inputFile.close();
+  //file.close();
+}
