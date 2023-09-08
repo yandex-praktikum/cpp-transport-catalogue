6fd42f5fcf929b7298d710ef5fb6ac21977ff204
PavelMalov <122271765+PavelMalov@users.noreply.github.com>
2023-09-08 12:06:25 +0300

    Update transport_catalogue.cpp

--- transport-catalogue/transport_catalogue.cpp
+++ transport-catalogue/transport_catalogue.cpp
@@ -2,13 +2,11 @@
 
 void TransportCatalogue::AddBus(std::string_view name, const std::vector<std::string_view>& stops) {
   std::vector<Stop*> tmp;
-  std::set<std::string_view> unique;
   for (auto stop : stops) {
     tmp.push_back(stop_name_to_stop.at(stop));
-    unique.insert(stop);
     stop_to_buses[stop].insert( (name));
   }
-  all_buses_.push_back({name, tmp, unique});
+  all_buses_.push_back({name, tmp});
   bus_name_to_bus[name] = &all_buses_.back();
 }
 void TransportCatalogue::AddStop(const Stop& stop) {
@@ -30,13 +28,11 @@ TransportCatalogue::Stop* TransportCatalogue::FindStop(std::string_view query) c
 
 TransportCatalogue::BusInfo TransportCatalogue::GetBusInfo(const Bus& bus)  {
   size_t stops = bus.route.size();
-  size_t unique = bus.unique_stops.size();
+  size_t unique = UniqueStops(bus.route);
   double length = 0;
   int real = 0;
   for (size_t i = 0; i < stops - 1; ++i) {
     length += ComputeDistance(bus.route[i]->coordinates, bus.route[i + 1]->coordinates);
-    ParseStops(*bus.route[i]);
-    ParseStops(*bus.route[i+1]);
     real+= ComputeRealDist(stop_name_to_stop.at(bus.route[i]->name), stop_name_to_stop.at(bus.route[i+1]->name));
   }
   return TransportCatalogue::BusInfo{stops, unique, real,length};
@@ -49,16 +45,25 @@ std::set<std::string_view> TransportCatalogue::GetStopInfo(const Stop& stop) {
   return empty;
   }
 
-void TransportCatalogue::ParseStops(const Stop& stop_)  {
-  std::string_view stop_name = stop_.name;
-  for(auto[stop, dist]:stop_.stops) {
-    distances_.insert({{stop_name_to_stop.at(stop_name), stop_name_to_stop.at(stop)},  dist});
-  }
 
-}
 int TransportCatalogue::ComputeRealDist(Stop* stop1, Stop* stop2) const{
   if (!distances_.count({stop1, stop2})) {
     return distances_.at({stop2, stop1});
   }
   return distances_.at({stop1, stop2});
 }
+
+size_t TransportCatalogue::UniqueStops(const std::vector<Stop*>& stops) {
+  std::vector<Stop*> uniq;
+  uniq.reserve(stops.size());
+  for(auto stop: stops) {
+    uniq.push_back(stop);
+  }
+  std::sort(uniq.begin(), uniq.end());
+  return std::unique(uniq.begin(), uniq.end()) - uniq.begin();
+}
+
+void TransportCatalogue::AddDistanceInfo(std::string_view stop1, std::string_view stop2, int dist) {
+  distances_[{stop_name_to_stop.at(stop1), stop_name_to_stop.at(stop2)}] = dist;
+}
+
