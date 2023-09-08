3e5b5c8f70bb56757f312b34b78ae1c7be62a813
PavelMalov <122271765+PavelMalov@users.noreply.github.com>
2023-09-08 12:05:41 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -2,10 +2,13 @@
 #include "input_reader.h"
 #include <iomanip>
 namespace output {
-void WriteBusInFile(std::ofstream& file, const std::string& query, TransportCatalogue& catalogue);
-void WriteBus(const std::string& query, TransportCatalogue& catalogue);
-void ParseQuery(const std::string& query, TransportCatalogue& catalogue);
-void ParseQueryFile(std::ofstream& file, const std::string& query, TransportCatalogue& catalogue);
-void WriteStopInFile(std::ofstream& file, const std::string& query, TransportCatalogue& catalogue);
-void WriteStop(const std::string& query, TransportCatalogue& catalogue);
+
+void WriteBus(std::ostream& out,const std::string& query,  TransportCatalogue& catalogue);
+
+void ParseOutput(std::ostream& out, const std::string& query,  TransportCatalogue& catalogue);
+
+
+void WriteStop(std::ostream& out, const std::string& query, TransportCatalogue& catalogue);
+
+void LoadOutputQueries(std::istream& in, std::ostream& out, TransportCatalogue& catalogue);
 }
