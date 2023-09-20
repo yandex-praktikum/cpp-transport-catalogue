f7fe95c378ddc6591e318cab7247f718ed8cad54
PavelMalov <122271765+PavelMalov@users.noreply.github.com>
2023-09-20 20:50:12 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,21 +1,19 @@
 #include <iostream>
-#include <ostream>
 
+
+#include "map_renderer.h"
 #include "json_reader.h"
 #include "request_handler.h"
-#include "map_renderer.h"
 
 int main() {
   json_input::Parser parser;
   TransportCatalogue catalogue;
-  std::ofstream out;
-  out.open("output.svg");
   auto document = json::Load(std::cin);
-  auto root = document.GetRoot().AsMap();
+  auto root = document.GetRoot().AsDict();
   auto input = root["base_requests"].AsArray();
   parser.LoadInputQueries(input, catalogue);
   auto output = root["stat_requests"].AsArray();
-  auto rend_info = root["render_settings"].AsMap();
+  auto rend_info = root["render_settings"].AsDict();
 
   auto ans = json_output::LoadOutputQueries(output, catalogue, rend_info);
   json::Print(json::Document{ans}, std::cout);
