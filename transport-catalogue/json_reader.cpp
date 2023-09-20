789d9e19cf21ee36baf95f030f57f73311d91fc3
PavelMalov <122271765+PavelMalov@users.noreply.github.com>
2023-09-20 20:51:11 +0300

    Update json_reader.cpp

--- transport-catalogue/json_reader.cpp
+++ transport-catalogue/json_reader.cpp
@@ -3,7 +3,7 @@
 namespace json_input {
 void Parser::LoadInputQueries(const json::Array& list, TransportCatalogue& catalogue) {
   for (const auto& one_query : list) {
-    ParseQuery(one_query.AsMap());
+    ParseQuery(one_query.AsDict());
   }
   ParseAllQueries(catalogue);
   ParseDistances(catalogue);
@@ -19,7 +19,7 @@ void Parser::ParseQuery(const json::Dict& query) {
 
 void Parser::ParseAllQueries(TransportCatalogue& catalogue) {
   for (const auto& query : deque_queries) {
-    auto tmp_query = query.AsMap();
+    auto tmp_query = query.AsDict();
     if (tmp_query.at("type").AsString() == "Bus") {
       if (tmp_query.at("is_roundtrip").AsBool()) {
         ParseBusQuery(tmp_query, catalogue);
@@ -64,7 +64,7 @@ void Parser::ParseStopQuery(const json::Dict& stop, TransportCatalogue& catalogu
   std::string_view name = all_queries.back();
   double latitude = stop.at("latitude").AsDouble();
   double longitude = stop.at("longitude").AsDouble();
-  auto distances = stop.at("road_distances").AsMap();
+  auto distances = stop.at("road_distances").AsDict();
   for (const auto& [kStop, dist] : distances) {
     all_queries.push_back(kStop);
     dist_[name].emplace_back(all_queries.back(), dist.AsInt());
@@ -83,28 +83,32 @@ void Parser::ParseDistances(TransportCatalogue& catalogue) {
 } // namespace json_input
 
 namespace json_output {
-void WriteBus(const json::Dict& bus, TransportCatalogue& catalogue, json::Array& ans) {
+void WriteBus(const json::Dict& bus, TransportCatalogue& catalogue, json::Builder& ans) {
   auto tmp_bus = catalogue.FindBus(bus.at("name").AsString());
   int id = bus.at("id").AsInt();
   if (tmp_bus == nullptr) {
-    ans.emplace_back(json::Dict{{"error_message", "not found"}, {"request_id", id}});
+    ans.StartDict().Key("error_message").Value("not found").Key("request_id").Value(id).EndDict();
+    //ans.emplace_back(json::Dict{{"error_message", "not found"}, {"request_id", id}});
     return;
   }
 
   auto info = catalogue.GetBusInfo(*tmp_bus);
-  ans.emplace_back(json::Dict{
+  ans.StartDict().Key("curvature").Value(info.real_lenght/info.route_length).Key("route_length").Value(info.real_lenght).Key("unique_stop_count").Value(static_cast<int>(info.unique_stops));
+  ans.Key("stop_count").Value(static_cast<int>(info.stops_on_route)).Key("request_id").Value(id).EndDict();
+ /* ans.emplace_back(json::Dict{
       {"curvature", info.real_lenght/info.route_length},
       {"route_length", info.real_lenght},
       {"unique_stop_count", static_cast<int>(info.unique_stops)},
       {"stop_count", static_cast<int>(info.stops_on_route)}, {"request_id",id }
-  });
+  });*/
 }
 
-void WriteStop(const json::Dict& stop, TransportCatalogue& catalogue, json::Array& ans) {
+void WriteStop(const json::Dict& stop, TransportCatalogue& catalogue, json::Builder& ans) {
   auto tmp_stop = catalogue.FindStop(stop.at("name").AsString());
   int id = stop.at("id").AsInt();
   if (tmp_stop == nullptr) {
-    ans.emplace_back(json::Dict{{"error_message", "not found"}, {"request_id", id}});
+    ans.StartDict().Key("error_message").Value("not found").Key("request_id").Value(id).EndDict();
+   // ans.emplace_back(json::Dict{{"error_message", "not found"}, {"request_id", id}});
     return;
   }
   auto buses = catalogue.GetStopInfo(*tmp_stop);
@@ -112,21 +116,24 @@ void WriteStop(const json::Dict& stop, TransportCatalogue& catalogue, json::Arra
   for(auto i : buses) {
     tmp.emplace_back(std::string(i));
   }
-  ans.emplace_back(json::Dict{{"buses", tmp}, {"request_id", id}});
+  ans.StartDict().Key("buses").Value(tmp).Key("request_id").Value(id).EndDict();
+  //ans.emplace_back(json::Dict{{"buses", tmp}, {"request_id", id}});
 }
 
-void WriteMap(const json::Dict& stop, TransportCatalogue& catalogue, json::Array& ans, const json::Dict& settings) {
+void WriteMap(const json::Dict& stop, TransportCatalogue& catalogue, json::Builder& ans, const json::Dict& settings) {
   int id = stop.at("id").AsInt();
   std::ostringstream out;
   render::LoadRenderInformation(out, catalogue, settings);
-  ans.emplace_back(json::Dict{{"map", out.str()} ,{"request_id", id}});
+  ans.StartDict().Key("map").Value(out.str()).Key("request_id").Value(id).EndDict();
+  //ans.emplace_back(json::Dict{{"map", out.str()} ,{"request_id", id}});
 
 }
 
-json::Array LoadOutputQueries(const json::Array& list, TransportCatalogue& catalogue, const json::Dict& settings) {
-  json::Array ans;
+json::Node LoadOutputQueries(const json::Array& list, TransportCatalogue& catalogue, const json::Dict& settings) {
+  json::Builder ans;
+  ans.StartArray();
   for (const auto& output : list) {
-    auto tmp_dict = output.AsMap();
+    auto tmp_dict = output.AsDict();
     if (tmp_dict.at("type").AsString() == "Bus") {
       WriteBus(tmp_dict, catalogue, ans);
     } else if (tmp_dict.at("type").AsString() == "Map") {
@@ -135,7 +142,8 @@ json::Array LoadOutputQueries(const json::Array& list, TransportCatalogue& catal
       WriteStop(tmp_dict, catalogue, ans);
     }
   }
-  return ans;
+  ans.EndArray();
+  return ans.Build();
 }
 
 
