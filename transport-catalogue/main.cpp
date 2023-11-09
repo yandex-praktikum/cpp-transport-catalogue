3399a913368d03d17e1dc397bcc903e1522c170d
PavelMalov <122271765+PavelMalov@users.noreply.github.com>
2023-11-09 16:45:41 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -4,6 +4,7 @@
 #include "map_renderer.h"
 #include "json_reader.h"
 #include "request_handler.h"
+#include "transport_router.h"
 
 int main() {
   json_input::Parser parser;
@@ -11,11 +12,19 @@ int main() {
   auto document = json::Load(std::cin);
   auto root = document.GetRoot().AsDict();
   auto input = root["base_requests"].AsArray();
+  auto graph_info = root["routing_settings"].AsDict();
+  parser.LoadRoutQuery(graph_info, catalogue);
   parser.LoadInputQueries(input, catalogue);
-  auto output = root["stat_requests"].AsArray();
-  auto rend_info = root["render_settings"].AsDict();
 
-  auto ans = json_output::LoadOutputQueries(output, catalogue, rend_info);
+
+
+auto output = root["stat_requests"].AsArray();
+auto rend_info = root["render_settings"].AsDict();
+
+
+TransportRouter router(catalogue);
+  auto ans = json_output::LoadOutputQueries(output, catalogue, rend_info, router);
   json::Print(json::Document{ans}, std::cout);
+
 
 }
