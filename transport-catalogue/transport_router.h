4e67f3ac907a5bcd894d9317807faf9a7b6e5c07
PavelMalov <122271765+PavelMalov@users.noreply.github.com>
2023-11-13 22:18:00 +0300

    Update transport_router.h

--- transport-catalogue/transport_router.h
+++ transport-catalogue/transport_router.h
@@ -3,45 +3,38 @@
 #include "transport_catalogue.h"
 #include "graph.h"
 #include "router.h"
+#include "json.h"
+
+
+
 
 
 class TransportRouter {
  public:
-  explicit TransportRouter(TransportCatalogue& catalogue) : catalogue_(catalogue){}
-
-  json::Dict FindRoute(std::string_view from, std::string_view to) {
-    auto cord = catalogue_.GetIds(from, to);
-    if (!cord.has_value()) {
-      return json::Dict {{"error_message", "not found"}};
-    }
-    auto [id_from, id_to] = cord.value();
-    auto info = router->BuildRoute(id_from, id_to);
-    if(!info.has_value()) {
-      return json::Dict {{"error_message", "not found"}};
-    }
-    json::Array items;
-   // auto tmp_edge = catalogue_.GetEdgeFromCatalogue(info->edges[0]);
-   auto time = catalogue_.GetWaitingTime();
-
-    for (auto edge: info->edges) {
-      items.emplace_back(json::Dict());
-      auto tmp_edge = catalogue_.GetEdgeFromCatalogue(edge);
-      auto stop_from = std::string(catalogue_.GetStringFromId(tmp_edge.from));
-
-      items.back() = json::Dict {{"stop_name", stop_from}, {"time", time}, {"type", "Wait"}};
-      auto name = std::string (tmp_edge.bus_name);
-      items.emplace_back(json::Dict());
-
-      items.back() = json::Dict{{"bus", name}, {"span_count", tmp_edge.stop_counter}, {"time", tmp_edge.weight - time}, {"type", "Bus"}};
-
-    }
-    return json::Dict {{"total_time", info->weight}, {"items", items}};
-
-
-
+  explicit TransportRouter(TransportCatalogue& catalogue) :catalogue_(catalogue) {
+    graph_.emplace(graph::DirectedWeightedGraph<double>(catalogue.GetAllStopsSize()));
   }
+
+  json::Dict FindRoute(std::string_view from, std::string_view to);
+    void PutRoutingInfo(int bus_velocity, int wait_time);
+
+  void ProcessCicledRout(const std::vector<TransportCatalogue::Stop*>& stops, std::string_view bus_name);
+
+  void ProcessNonCycleRout(const std::vector<TransportCatalogue::Stop*>& stops, std::string_view bus_name);
+
+
+  void ProcessGraph();
+
+  std::optional<std::pair<size_t, size_t>> GetIds (std::string_view stop1, std::string_view stop2);
 
  private:
   TransportCatalogue& catalogue_;
-  std::optional<graph::Router<double>> router = catalogue_.ProcessGraph();
+  std::optional<graph::Router<double>> router;
+
+  std::unordered_map<std::string_view, size_t> stop_to_id ;
+  std::unordered_map<size_t , std::string_view> id_to_stop;
+
+
+  int velocity_ = 0, wait_time_ = 0;
+  std::optional<graph::DirectedWeightedGraph<double>> graph_;
 };
