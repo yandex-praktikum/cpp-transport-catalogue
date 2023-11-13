7fb6a7bec72867c01a56e7ec4d26ba22824a9baf
PavelMalov <122271765+PavelMalov@users.noreply.github.com>
2023-11-13 22:16:30 +0300

    Update transport_catalogue.cpp

--- transport-catalogue/transport_catalogue.cpp
+++ transport-catalogue/transport_catalogue.cpp
@@ -46,7 +46,7 @@ std::set<std::string_view> TransportCatalogue::GetStopInfo(const Stop& stop) {
   return empty;
 }
 
-int TransportCatalogue::ComputeRealDist(Stop* stop1, Stop* stop2) const {
+int TransportCatalogue::ComputeRealDist( Stop* stop1, Stop* stop2) const {
   if (!distances_.count({stop1, stop2})) {
     return distances_.at({stop2, stop1});
   }
@@ -74,64 +74,4 @@ std::set<std::string_view> TransportCatalogue::GetAllBuses() const {
     ans.insert(bus.name);
   }
   return ans;
-}
-
-graph::Router<double> TransportCatalogue::ProcessGraph() {
-  graph_ = graph::DirectedWeightedGraph<double>(all_stops_.size());
-  for (const auto& bus : all_buses_) {
-
-    if (bus.is_roundtrip) {
-      ProcessCicledRout(bus.route, bus.name);
-    } else {
-      ProcessNonCycleRout(bus.route, bus.name);
-    }
-  }
-
-  return graph::Router(graph_);
-}
-
-void TransportCatalogue::ProcessCicledRout(const std::vector<Stop*>& stops, std::string_view bus_name) {
-  size_t id = id_to_stop.size();
-  std::set<std::pair<size_t, size_t >> unique_stops;
-  for (int i = 0; i < int(stops.size()) - 1; ++i) {
-    if (!stop_to_id.count(stops[i]->name)) {
-      stop_to_id[stops[i]->name] = id;
-      id_to_stop[id++] = stops[i]->name;
-    }
-    double total_dist_for_i_bus = 0;
-    auto stop1 = stop_name_to_stop.at(stops[i]->name);
-    auto from = stop_to_id.at(stop1->name);
-    for (int j = i + 1; j < stops.size(); ++j) {
-
-      if (!stop_to_id.count(stops[j]->name)) {
-        stop_to_id[stops[j]->name] = id;
-        id_to_stop[id++] = stops[j]->name;
-      }
-
-
-      auto stop2 = stop_name_to_stop.at(stops[j]->name);
-
-
-      auto to = stop_to_id.at(stop2->name);
-      unique_stops.insert({from, to});
-      total_dist_for_i_bus += ComputeRealDist(stop1, stop2);
-      graph_.AddEdge({from, to, ((total_dist_for_i_bus / 1000) / velocity_)* 60  + wait_time_ , bus_name,j - i});
-      stop1 = stop2;
-    }
-  }
-}
-
-void TransportCatalogue::ProcessNonCycleRout(const std::vector<Stop*>& stops, std::string_view bus_name) {
-  long n = long(stops.size());
-  auto med = (n / 2) + 1;
-  std::vector<Stop*> tmp;
-  for (int i = 0; i < med; ++i) {
-    tmp.push_back(stops[i]);
-  }
-  ProcessCicledRout(tmp, bus_name);
-  tmp.clear();
-  for (int i = med - 1; i < n; ++i) {
-    tmp.push_back(stops[i]);
-  }
-  ProcessCicledRout(tmp, bus_name);
 }
