40a08a125e27d37a932c17ff4947e8bc800d7e53
PavelMalov <122271765+PavelMalov@users.noreply.github.com>
2023-11-13 22:16:52 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -57,59 +57,18 @@ class TransportCatalogue {
   std::set<std::string_view > GetAllBuses() const;
 
 
-  void PutRoutingInfo(int bus_velocity, int wait_time) {
-    velocity_ = bus_velocity;
-    wait_time_ = wait_time;
-  }
 
-  graph::Router<double> ProcessGraph() ;
-
-
-void PrintRoute(const std::optional<graph::Router<double>::RouteInfo>& info) const {
-  if (!info.has_value()) {
-    std::cout << "No WAY!\n";
-    return;
-  }
-  bool flag = false;
-  for (auto item: info->edges) {
-    auto edges = graph_.GetEdge(item);
-    if (!flag) {
-      std::cout << id_to_stop.at(edges.from) << " " << id_to_stop.at(edges.to) << " " << edges.stop_counter << " " << edges.weight << " " ;
-    }
-    else {
-      std::cout << id_to_stop.at(edges.to) << " " <<edges.stop_counter << " " << edges.weight << ' ';
-    }
-    flag = true;
-
-  }
-  std::cout << "\n" << info->weight << "\n";
-
+size_t GetAllStopsSize() const{
+  return all_stops_.size();
+}
+const std::list<Bus>& GetAllBusesForRoute() const {
+  return all_buses_;
 }
 
-std::optional<std::pair<size_t, size_t>> GetIds (std::string_view stop1, std::string_view stop2) {
-  std::optional<std::pair<size_t , size_t >> ans;
-  if (!stop_to_id.count(stop1)) {
-    return ans;
-  }
-  if (!stop_to_id.count(stop2)) {
-    return ans;
-  }
-  ans = {stop_to_id.at(stop1), stop_to_id.at(stop2)};
-  return ans;
+ Stop* GetStopFromString(std::string_view stop_name) const {
+  return stop_name_to_stop.at(stop_name);
 }
-
-graph::Edge<double> GetEdgeFromCatalogue(size_t id) const {
-  return graph_.GetEdge(id);
-}
-std::string_view GetStringFromId(size_t id) const {
-  return id_to_stop.at(id);
-}
-
-int GetWaitingTime() const {
-  return wait_time_;
-}
-
-
+ int ComputeRealDist( Stop* stop1,  Stop* stop2) const;
  private:
 
 
@@ -121,22 +80,10 @@ int GetWaitingTime() const {
   std::unordered_map<std::string_view , std::set<std::string_view>> stop_to_buses;
   std::unordered_map<PairStop, int, Hasher> distances_;
 
-  graph::DirectedWeightedGraph<double> graph_;
-  int velocity_ = 0, wait_time_ = 0;
 
-  std::unordered_map<std::string_view, size_t> stop_to_id;
-  std::unordered_map<size_t , std::string_view> id_to_stop;
-
-
-
-
-  int ComputeRealDist(Stop* stop1, Stop* stop2) const;
 
   size_t UniqueStops(const std::vector<Stop*>& stops);
 
-  void ProcessCicledRout(const std::vector<Stop*>& stops, std::string_view bus_name);
-
-  void ProcessNonCycleRout(const std::vector<Stop*>& stops, std::string_view bus_name);
 
 
 
